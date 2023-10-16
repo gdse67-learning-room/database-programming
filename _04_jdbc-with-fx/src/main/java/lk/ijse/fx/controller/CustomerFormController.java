0f00a28ded051c6b139625571696263d9e942515
danujav <danujagreru@gmail.com>
2023-10-16 11:43:38 +0530

    customer search done with clear fields

--- _04_jdbc-with-fx/src/main/java/lk/ijse/fx/controller/CustomerFormController.java
+++ _04_jdbc-with-fx/src/main/java/lk/ijse/fx/controller/CustomerFormController.java
@@ -5,10 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class CustomerFormController {
     @FXML
@@ -32,7 +29,7 @@ public class CustomerFormController {
 
         try {
             Connection connection = DriverManager.getConnection(
-                    "jdbc:mysq://localhost:3306/kade2",
+                    "jdbc:mysql://localhost:3306/kade2",
                     "root",
                     "Sachi@123"
             );
@@ -48,10 +45,52 @@ public class CustomerFormController {
             int affectedRows = pstm.executeUpdate();
 
             if(affectedRows > 0) {
+                clearFields();
                 new Alert(Alert.AlertType.CONFIRMATION, "customer saved!!").show();
             }
 
             connection.close();
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+
+    private void clearFields() {
+        txtId.setText("");
+        txtName.setText("");
+        txtAddress.setText("");
+        txtTel.setText("");
+    }
+
+    @FXML
+    void txtSearchOnAction(ActionEvent event) {
+        String id = txtId.getText();
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/kade2",
+                    "root",
+                    "Sachi@123"
+            );
+
+            String sql = "SELECT * FROM customer WHERE id=?";
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setString(1, id);
+
+            ResultSet resultSet = pstm.executeQuery();
+
+            if(resultSet.next()) {
+                String cus_id = resultSet.getString(1);
+                String cus_name = resultSet.getString(2);
+                String cus_address = resultSet.getString(3);
+                String cus_tel = resultSet.getString(4);
+
+                txtName.setText(cus_name);
+                txtAddress.setText(cus_address);
+                txtTel.setText(cus_tel);
+            } else {
+                new Alert(Alert.AlertType.INFORMATION, "cutomer not found!").show();
+            }
         } catch (SQLException e) {
             new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
         }
