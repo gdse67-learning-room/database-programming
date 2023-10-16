9990b7c86baa50199a45dfdb12448e9a5b1d5b71
danujav <danujagreru@gmail.com>
2023-10-16 11:29:58 +0530

    customer added success

--- _04_jdbc-with-fx/src/main/java/lk/ijse/fx/controller/CustomerFormController.java
+++ _04_jdbc-with-fx/src/main/java/lk/ijse/fx/controller/CustomerFormController.java
@@ -1,4 +1,59 @@
 package lk.ijse.fx.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CustomerFormController {
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtTel;
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        String tel = txtTel.getText();
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysq://localhost:3306/kade2",
+                    "root",
+                    "Sachi@123"
+            );
+
+            String sql = "INSERT INTO customer VALUES(?, ?, ?, ?)";
+            PreparedStatement pstm = connection.prepareStatement(sql);
+
+            pstm.setString(1, id);
+            pstm.setString(2, name);
+            pstm.setString(3, address);
+            pstm.setString(4, tel);
+
+            int affectedRows = pstm.executeUpdate();
+
+            if(affectedRows > 0) {
+                new Alert(Alert.AlertType.CONFIRMATION, "customer saved!!").show();
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
 }
