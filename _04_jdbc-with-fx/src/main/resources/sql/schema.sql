d32a8665d39194c107a95b0c8bd1165801df11d6
danujav <danujagreru@gmail.com>
2023-10-16 16:04:02 +0530

    add DbConnection.

--- _04_jdbc-with-fx/src/main/resources/sql/schema.sql
+++ _04_jdbc-with-fx/src/main/resources/sql/schema.sql
@@ -10,3 +10,10 @@ CREATE TABLE customer(
 	address TEXT NOT NULL,
 	tel VARCHAR(15) NOT NULL
 );
+
+CREATE TABLE item(
+    code VARCHAR(35) PRIMARY KEY,
+    description TEXT NOT NULL,
+    unit_price DOUBLE NOT NULL,
+    qty_on_hand INT NOT NULL
+);
